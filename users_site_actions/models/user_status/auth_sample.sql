a681c4e98306dc4205f44dec51e62a3f2508e0ba
korbash <korbash.sen@gmail.com>
2023-09-25 12:55:18 +0300

    add all events with user_id to user status

--- users_site_actions/models/user_status/auth_sample.sql
+++ users_site_actions/models/user_status/auth_sample.sql
@@ -4,17 +4,18 @@ SELECT max(toDate(time_start)) AS last_date FROM track_user_status
 {% if execute %}
     {% if run_query('EXISTS TABLE track_user_status').columns[0].values()[0] %}
         {% set max_date = run_query(get_max_date).columns[0].values()[0] %}
-    {% else %}
-        {% set max_date = '2022-09-01' %}
+    {% else %} {% set max_date = '2022-09-01' %}
     {% endif %}
 {% endif %}
 
 
-
-select * 
+select *
 from {{ source('src_actions', 'src_actions') }}
-where event == 'auth'
-  AND success
-  AND isNotNull(user_id)
-  AND toDate(time_sort) BETWEEN toDate('{{ max_date }}') + toIntervalDay(1) AND toDate('{{ max_date }}') + toIntervalMonth(3)
-
+where
+    event in ['auth', 'trade', 'pet2game', 'payment']
+    and success
+    and isNotNull(user_id)
+    and toDate(time_sort)
+    between toDate('{{ max_date }}')
+    + toIntervalDay(1) and toDate('{{ max_date }}')
+    + toIntervalMonth(1)
